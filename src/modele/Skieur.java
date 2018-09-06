85d106cf03e9041697a24591e043c43b4f767e6a
paul-jacobs <jacobs.paul.pro@gmail.com>
2018-09-05 22:16:15 -0400

    feat(Skieur.java) : getters ajout getters

--- src/modele/Skieur.java
+++ src/modele/Skieur.java
@@ -6,27 +6,62 @@ public class Skieur {
 	protected String age;
 	protected String poids;
 	
+	
+	
+	public Skieur(String nom) {
+		super();
+		this.nom = nom;
+	}
+	
+	public Skieur(String nom, String prenom) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+	
+	public Skieur(String nom, String prenom, String age) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.age = age;
+	}
+
+	public Skieur(String nom, String prenom, String age, String poids) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.age = age;
+		this.poids = poids;
+	}
+
 	public String getNom() {
 		return nom;
 	}
+	
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+	
 	public String getPrenom() {
 		return prenom;
 	}
+	
 	public void setPrenom(String prenom) {
 		this.prenom = prenom;
 	}
+	
 	public String getAge() {
 		return age;
 	}
+	
 	public void setAge(String age) {
 		this.age = age;
 	}
+	
 	public String getPoids() {
 		return poids;
 	}
+	
 	public void setPoids(String poids) {
 		this.poids = poids;
 	}
