0079f5e45b8eb258f2dd03e189c0e335c7d942e1
paul-jacobs <jacobs.paul.pro@gmail.com>
2018-09-05 22:10:27 -0400

    feat(Modele) : ajout Modele

---
+++ src/modele/Skieur.java
@@ -0,0 +1,34 @@
+package modele;
+
+public class Skieur {
+ 	protected String nom;
+	protected String prenom;
+	protected String age;
+	protected String poids;
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getPoids() {
+		return poids;
+	}
+	public void setPoids(String poids) {
+		this.poids = poids;
+	}
+	
+}
