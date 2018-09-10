dd3c4135f045173fd23663444f900a24068cb4cd
paul-jacobs <jacobs.paul.pro@gmail.com>
2018-09-10 00:29:52 -0400

    feat (DAO) : ajout connexion bdd postgre

--- src/donnee/SkieurDAO.java
+++ src/donnee/SkieurDAO.java
@@ -2,12 +2,15 @@ package donnee;
 
 import modele.Skieur;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SkieurDAO {
 
     private List<Skieur> simulerListeSkieur(){
+
         List<Skieur> listeSkieur = new ArrayList<Skieur>();
 
         listeSkieur.add(new Skieur("Jacobs", "paul", "18 ans", "70 kg"));
@@ -17,6 +20,24 @@ public class SkieurDAO {
     }
 
     public List<Skieur> listerSkieur() {
+
+        String BASEDEDONNEES_DRIVER = "org.postgresql.Driver";
+        String BASEDEDONNEES_URL = "jdbc:postgresql://localhost:5432/skieur";
+        String BASEDEDONNEES_USAGER = "postgres";
+        String BASEDEDONNEES_MOTDEPASSE = "toto";
+
+        try {
+            Class.forName(BASEDEDONNEES_DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            DriverManager.getConnection(BASEDEDONNEES_URL, BASEDEDONNEES_USAGER, BASEDEDONNEES_MOTDEPASSE);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         return simulerListeSkieur();
     }
 }
