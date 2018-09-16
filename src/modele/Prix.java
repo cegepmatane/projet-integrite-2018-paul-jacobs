1b0ca4e54556038bf2cadaba70492773293110aa
paul-jacobs <jacobs.paul.pro@gmail.com>
2018-09-16 16:38:21 -0400

    feat (afficherPrix): ajout de l'affichage du tableau de prix + ajout de champs dans Prix

--- src/modele/Prix.java
+++ src/modele/Prix.java
@@ -5,6 +5,8 @@ public class Prix {
     private String idPrix;
     private String nomPrix;
     private String datePrix;
+    private String temps;
+    private String place;
 
     public Prix(String idPrix) {
         this.idPrix = idPrix;
@@ -19,6 +21,21 @@ public class Prix {
         this.idPrix = idPrix;
         this.nomPrix = nomPrix;
         this.datePrix = datePrix;
+    }
+
+    public Prix(String idPrix, String nomPrix, String datePrix, String temps) {
+        this.idPrix = idPrix;
+        this.nomPrix = nomPrix;
+        this.datePrix = datePrix;
+        this.temps = temps;
+    }
+
+    public Prix(String idPrix, String nomPrix, String datePrix, String temps, String place) {
+        this.idPrix = idPrix;
+        this.nomPrix = nomPrix;
+        this.datePrix = datePrix;
+        this.temps = temps;
+        this.place = place;
     }
 
     public String getIdPrix() {
@@ -43,5 +60,21 @@ public class Prix {
 
     public void setDatePrix(String datePrix) {
         this.datePrix = datePrix;
+    }
+
+    public String getTemps() {
+        return temps;
+    }
+
+    public void setTemps(String temps) {
+        this.temps = temps;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
     }
 }
--- src/vue/VueModifierSkieur.java
+++ src/vue/VueModifierSkieur.java
@@ -14,6 +14,8 @@ import modele.Skieur;
 public class VueModifierSkieur extends Scene {
 
     private Button actionModifierSkieur;
+    private GridPane grilleAffichage;
+    private GridPane grilleAffichagePrix;
     private GridPane grilleModifierSkieur;
 
     private Controleur controleur;
@@ -24,7 +26,10 @@ public class VueModifierSkieur extends Scene {
 
     public VueModifierSkieur() {
         super(new GridPane(),400,400);
-        this.grilleModifierSkieur = (GridPane) this.getRoot();
+        this.grilleAffichage = (GridPane) this.getRoot();
+
+        this.grilleModifierSkieur = new GridPane();
+        this.grilleAffichagePrix = new GridPane();
 
         this.actionModifierSkieur = new Button("enregistrer");
 
@@ -39,7 +44,9 @@ public class VueModifierSkieur extends Scene {
 
     public void afficherGrillerModifierSkieur(Skieur skieur)
     {
+        this.grilleAffichage.getChildren().clear();
         this.grilleModifierSkieur.getChildren().clear();
+        this.grilleAffichagePrix.getChildren().clear();
         this.grilleModifierSkieur.add(new Label("Nom : "), 0, 0);
         this.grilleModifierSkieur.add(new Label("Prenom : "), 0, 1);
         this.grilleModifierSkieur.add(new Label("Age : "), 0, 2);
@@ -54,8 +61,28 @@ public class VueModifierSkieur extends Scene {
         this.grilleModifierSkieur.add(valeurPrenom,1,1);
         this.grilleModifierSkieur.add(valeurAge,1,2);
         this.grilleModifierSkieur.add(valeurPoids,1,3);
-        this.grilleModifierSkieur.add(actionModifierSkieur,0,4);
+        this.grilleAffichage.add(grilleModifierSkieur,0,0);
+        afficherGrillePrix();
+        this.grilleAffichage.add(actionModifierSkieur,0,5);
 
+    }
+
+    private void afficherGrillePrix()
+    {
+        grilleAffichagePrix.add(new Label("nom"),0,0);
+        grilleAffichagePrix.add(new Label("date"),1,0);
+        grilleAffichagePrix.add(new Label("temps"),2,0);
+        grilleAffichagePrix.add(new Label("place"),3,0);
+
+        grilleAffichagePrix.add(new Label("prix1"), 0,1);
+        grilleAffichagePrix.add(new Label("00-00-0000"), 1,1);
+        grilleAffichagePrix.add(new Label("1'10"), 2,1);
+        grilleAffichagePrix.add(new Label("1"), 3,1);
+
+        grilleAffichagePrix.add(new Button("modifier"), 4, 1);
+        grilleAffichagePrix.add(new Button("supprimer"), 5, 1);
+
+        this.grilleAffichage.add(grilleAffichagePrix,0,4);
     }
 
     private Skieur recupererSkieur(){
